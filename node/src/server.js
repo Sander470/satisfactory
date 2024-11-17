32d1fc6dea7079b24ff54e259950647586ad9cc2
alex <sander-grootjans@hotmail.com>
2024-11-17 06:37:03 +0100

    start work on website

--- node/src/server.js
+++ node/src/server.js
@@ -1,6 +1,18 @@
 import express from "express";
 const app = express();
 
+const login = {
+}
+
 app.get('/', (req, res) => {
-    res.send("<h3>hi</h3>");
+    res.send("<h3>not an intended route.</h3>");
+    console.log('attempt access route at /');
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        const pwd_hash = await 
+    } catch (error) {
+        
+    }
 })
