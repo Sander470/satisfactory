c98279d1be0f8913364b9b4118b1572782ae134d
alex <sander-grootjans@hotmail.com>
2024-11-21 05:12:54 +0100

    main controls done

---
+++ nginx/site/assets/scripts/button.js
@@ -0,0 +1,26 @@
+function makeCall(button) {
+    const buttonName = button.name;
+    let confirm = document.getElementById("confirm")
+
+    switch (buttonName) {
+        case "refresh":
+            confirm.innerHTML = "refresh";
+            break;
+        
+        case "start":
+            confirm.innerHTML = "start"
+            break;
+        
+        case "stop":
+            confirm.innerHTML = "stop"
+            break;
+        
+        case "restart":
+            confirm.innerHTML = "restart"
+            break;
+
+    
+        default:
+            break;
+    }
+}
